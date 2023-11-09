0212b2a67aba6ac9d6a276b14bd162e27cbfc172
Javier Tapia <jgtapiab@telconet.ec>
2023-11-08 22:19:34 -0500

    Initial commit

---
+++ src/main/java/udemy/course/spring6dependencyinjection/Spring6DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package udemy.course.spring6dependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6DependencyInjectionApplication.class, args);
+    }
+
+}
